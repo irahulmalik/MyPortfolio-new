021768b476d8bc93b69a866ff83726813a26b2e3
irahulmalik <rahulmalik1995@gmail.com>
2022-05-31 15:01:16 +0530

    added home html

--- src/app/components/home/home.componenet.ts
+++ src/app/components/home/home.componenet.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [`./home.component.scss`],
 })
 export class HomeComponent implements OnInit {
+  menuOptions:HTMLElement;
   constructor() {}
-  ngOnInit() {}
+  ngOnInit() {
+    this.menuOptions = document.getElementById("menu-options");
+  }
 
   toggleMenu(){
-    console.log("toggle")
+    console.log("toggle",this.menuOptions.classList)
+    if(this.menuOptions.classList.contains("hidden")){
+      this.menuOptions.classList.remove("hidden")
+    }
+    else{
+      this.menuOptions.classList.add("hidden")
+    }
   }
 }
