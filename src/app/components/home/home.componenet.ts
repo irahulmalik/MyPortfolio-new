463b84bf90cb511eef72a02350c7da2d6ad9b212
irahulmalik <rahulmalik1995@gmail.com>
2022-05-31 22:08:54 +0530

    working on responsive menu

--- src/app/components/home/home.componenet.ts
+++ src/app/components/home/home.componenet.ts
@@ -7,18 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   menuOptions:HTMLElement;
+  hamburgerOption:HTMLElement;
   constructor() {}
   ngOnInit() {
     this.menuOptions = document.getElementById("menu-options");
+    this.hamburgerOption = document.getElementById("menu-options-hamburger");
   }
 
   toggleMenu(){
     console.log("toggle",this.menuOptions.classList)
     if(this.menuOptions.classList.contains("hidden")){
       this.menuOptions.classList.remove("hidden")
+      this.hamburgerOption.classList.add("hidden");
     }
     else{
       this.menuOptions.classList.add("hidden")
+      this.hamburgerOption.classList.remove("hidden");
     }
   }
 }
